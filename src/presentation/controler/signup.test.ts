a91e48441b063ea7dc75e1d2696e208b842f6223
Pedro <jotahpeee@live.com>
2023-04-12 22:33:42 -0300

    test: first use of jest.spyOn to mock result of emailValidator

--- src/presentation/controler/signup.test.ts
+++ src/presentation/controler/signup.test.ts
@@ -1,14 +1,32 @@
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { customCodeBadRequest } from '../helper/http-helper'
+import { type EmailValidator } from '../protocols/email-validator'
 import { SignUpController } from './signup'
 
-const makeSut = (): SignUpController => {
-  return new SignUpController()
+interface SutTypes {
+  sut: SignUpController
+  emailValidator: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidator = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidator)
+  return {
+    sut,
+    emailValidator
+  }
 }
 
 describe('Signup Controller', () => {
   test('should return 400 if no name is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body:
       {
@@ -25,12 +43,12 @@ describe('Signup Controller', () => {
 
 describe('Signup Controller', () => {
   test('should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body:
       {
         name: 'any',
-        // mail: 'mail@mail.com',
+        // email: 'mail@mail.com'
         password: 'anypass'
       }
     }
@@ -42,17 +60,35 @@ describe('Signup Controller', () => {
 
 describe('Signup Controller', () => {
   test('should return 400 if no password is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body:
       {
         name: 'any',
-        mail: 'mail@mail.com'
+        email: 'mail@mail.com'
         // password: 'anypass'
       }
     }
     const httpReponse = sut.handle(httpRequest)
     expect(httpReponse.statusCode).toBe(400)
-    expect(httpReponse.body).toEqual(customCodeBadRequest(3000, MissingParamError.name, new MissingParamError('email').message, 400).body)
+    expect(httpReponse.body).toEqual(customCodeBadRequest(3000, MissingParamError.name, new MissingParamError('password').message, 400).body)
+  })
+})
+
+describe('Signup Controller', () => {
+  test('should return 400 if invalid mail is provided', () => {
+    const { sut, emailValidator } = makeSut()
+    jest.spyOn(emailValidator, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body:
+      {
+        name: 'any',
+        email: 'invalid mail',
+        password: 'anypass'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+    expect(httpReponse.body).toEqual(customCodeBadRequest(3001, InvalidParamError.name, new InvalidParamError('email').message, 400).body)
   })
 })
