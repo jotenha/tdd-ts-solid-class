1554b6c1ccd992f26f1586224956023012dcee45
Pedro <jotahpeee@live.com>
2023-04-12 21:58:31 -0300

    feat: added base signup controller

---
+++ src/presentation/controler/signup.ts
@@ -0,0 +1,20 @@
+import { MissingParamError } from '../errors/missing-param-error'
+import { customCodeBadRequest } from '../helper/http-helper'
+import { type HttpResponse, type HttpRequest } from '../protocols/http'
+
+export class SignUpController {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return customCodeBadRequest(3000, MissingParamError.name, new MissingParamError(field).message, 400)
+      }
+    }
+    return {
+      statusCode: 200,
+      body: {
+        message: 'Sign-up successful'
+      }
+    }
+  }
+}
