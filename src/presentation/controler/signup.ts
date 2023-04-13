3b8aab49c540c4e3052ee51273188ead6e1d9f0a
Pedro <jotahpeee@live.com>
2023-04-12 22:33:38 -0300

    feat: first use of jest.spyOn to mock result of emailValidator

--- src/presentation/controler/signup.ts
+++ src/presentation/controler/signup.ts
@@ -1,15 +1,30 @@
+import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { customCodeBadRequest } from '../helper/http-helper'
 import { type Controller } from '../protocols/controller'
+import { type EmailValidator } from '../protocols/email-validator'
 import { type HttpResponse, type HttpRequest } from '../protocols/http'
 
 export class SignUpController implements Controller {
+  /* emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  } */
+  constructor (
+    private readonly emailvalidator: EmailValidator
+  ) {}
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return customCodeBadRequest(3000, MissingParamError.name, new MissingParamError(field).message, 400)
       }
+    }
+    const { email } = httpRequest.body
+    const isMailValid = this.emailvalidator.isValid(email)
+    if (!isMailValid) {
+      return customCodeBadRequest(3001, InvalidParamError.name, new InvalidParamError('email').message, 400)
     }
     return {
       statusCode: 200,
