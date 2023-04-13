70e8a63f1d60bf265307680bab5c08479aaa66bc
Pedro <jotahpeee@live.com>
2023-04-12 21:58:16 -0300

    feat: added custom error returning

---
+++ src/presentation/helper/http-helper.ts
@@ -0,0 +1,16 @@
+import { type HttpResponse } from '../protocols/http'
+
+export const customCodeBadRequest = (errorcode: number, errorName: string, errorMessage: string, status?: number): HttpResponse => {
+  let sCode = 400
+  if (status !== undefined) {
+    sCode = status
+  }
+  return {
+    statusCode: sCode,
+    body: {
+      code: errorcode,
+      error: errorName,
+      message: errorMessage
+    }
+  }
+}
