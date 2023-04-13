3dcee362af48b361a948d23450ebb869a649a103
Pedro <jotahpeee@live.com>
2023-04-12 22:52:38 -0300

    feat: added exception handling if dep throws

--- src/presentation/controler/signup.ts
+++ src/presentation/controler/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { customCodeBadRequest } from '../helper/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
@@ -15,21 +16,28 @@ export class SignUpController implements Controller {
   ) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return customCodeBadRequest(3000, MissingParamError.name, new MissingParamError(field).message, 400)
+    try {
+      const requiredFields = ['name', 'email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return customCodeBadRequest(3000, MissingParamError.name, new MissingParamError(field).message, 400)
+        }
       }
-    }
-    const { email } = httpRequest.body
-    const isMailValid = this.emailvalidator.isValid(email)
-    if (!isMailValid) {
-      return customCodeBadRequest(3001, InvalidParamError.name, new InvalidParamError('email').message, 400)
-    }
-    return {
-      statusCode: 200,
-      body: {
-        message: 'Sign-up successful'
+      const { email } = httpRequest.body
+      const isMailValid = this.emailvalidator.isValid(email)
+      if (!isMailValid) {
+        return customCodeBadRequest(3001, InvalidParamError.name, new InvalidParamError('email').message, 400)
+      }
+      return {
+        statusCode: 200,
+        body: {
+          message: 'Sign-up successful'
+        }
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
   }
